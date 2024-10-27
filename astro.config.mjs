318c6ef01c9f0d4fe504b110e88c69446a3c1719
ihsanvp <ihsanvp2738@gmail.com>
2024-10-27 23:53:26 +0530

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://reverse-rain.github.io',
-  base: 'artigence',
-});
+export default defineConfig({});
